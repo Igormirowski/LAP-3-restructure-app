796e6e44c23dc8b57f3bb6436eeab39aac1859e7
getfutureproof-admin <admin@getfutureproof.co.uk>
2020-11-23 12:42:02 +0000

    re-add sinon

--- src/test/setupTests.js
+++ src/test/setupTests.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import sinon from 'sinon';
+import sinon from 'sinon';
 import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
@@ -7,4 +7,4 @@ configure({ adapter: new Adapter() });
 
 global.React = React;
 global.shallow = shallow;
-// global.sinon = sinon;
+global.sinon = sinon;
