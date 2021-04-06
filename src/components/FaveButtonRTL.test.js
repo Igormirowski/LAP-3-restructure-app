fe37adf35bbeb294d46b61ab7587185c55a98103
getfutureproof-admin <admin@getfutureproof.co.uk>
2021-04-06 16:22:57 +0100

    add first rtl test

--- src/components/FaveButtonRTL.test.js
+++ src/components/FaveButtonRTL.test.js
@@ -1,23 +1,27 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import "@testing-library/jest-dom/extend-expect";
+
 import FaveButton from './FaveButton';
 
 describe('FaveButton', () => {
     let component;
 
     beforeEach(() => {
-        component = shallow(<FaveButton />)
+        component = render(<FaveButton />)
     })
 
     test('renders a span with a star (★) in it', () => {
-        let starSpan = component.find('span')
-        expect(starSpan.text()).toContain('★')
+        let starSpan = component.getByRole("switch")
+        expect(starSpan.textContent).toBe('★')
     })
 
-    test('changes colour of star when clicked', () => {
-        let fakeEvent = { stopPropagation: () => "do nothing" }
-        let starSpan = component.find('span')
-        let initColour = starSpan.prop('style').color
-        starSpan.simulate('click', fakeEvent)
-        let clickedColour = component.find('span').prop('style').color
-        expect(clickedColour).not.toBe(initColour)
-    })
+    // test('changes colour of star when clicked', () => {
+    //     let fakeEvent = { stopPropagation: () => "do nothing" }
+    //     let starSpan = component.find('span')
+    //     let initColour = starSpan.prop('style').color
+    //     starSpan.simulate('click', fakeEvent)
+    //     let clickedColour = component.find('span').prop('style').color
+    //     expect(clickedColour).not.toBe(initColour)
+    // })
 })
