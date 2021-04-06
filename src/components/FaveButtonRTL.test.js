02a800fc99f3ed251fe9c689e73d493aefdd7d71
getfutureproof-admin <admin@getfutureproof.co.uk>
2021-04-06 16:04:08 +0100

    create dup FaveButton test file

---
+++ src/components/FaveButtonRTL.test.js
@@ -0,0 +1,23 @@
+import FaveButton from './FaveButton';
+
+describe('FaveButton', () => {
+    let component;
+
+    beforeEach(() => {
+        component = shallow(<FaveButton />)
+    })
+
+    test('renders a span with a star (★) in it', () => {
+        let starSpan = component.find('span')
+        expect(starSpan.text()).toContain('★')
+    })
+
+    test('changes colour of star when clicked', () => {
+        let fakeEvent = { stopPropagation: () => "do nothing" }
+        let starSpan = component.find('span')
+        let initColour = starSpan.prop('style').color
+        starSpan.simulate('click', fakeEvent)
+        let clickedColour = component.find('span').prop('style').color
+        expect(clickedColour).not.toBe(initColour)
+    })
+})
